7cfe3889406067d98104459157d72b24176336c8
Isaac - Exon <isaac.souza@exongenomics.com>
2024-08-11 19:16:07 -0300

    construção para login com google

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useEffect, useState, useRef, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context.tsx/AuthContext";
 import api from "../../services/api";
@@ -21,11 +21,33 @@ const Login: React.FC = () => {
     useState(false);
   const [resetEmail, setResetEmail] = useState("");
 
+  const handleGoogleToken = useCallback(
+    (token: string) => {
+      try {
+        localStorage.setItem("token", token);
+
+        const user = {};
+
+        login(token, user);
+        navigate("/TelaInicial");
+      } catch (error) {
+        console.error("Erro ao processar o login com o Google:", error);
+        setError("Erro ao processar o login com o Google.");
+      }
+    },
+    [login, navigate]
+  );
+
   useEffect(() => {
-    if (passwordRef.current) {
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get("token");
+
+    if (token) {
+      handleGoogleToken(token);
+    } else if (passwordRef.current) {
       passwordRef.current.focus();
     }
-  }, []);
+  }, [handleGoogleToken]);
 
   const handleUserChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
@@ -78,6 +100,10 @@ const Login: React.FC = () => {
     }
   };
 
+  const handleGoogleLogin = () => {
+    window.location.href = `${api.defaults.baseURL}/auth/google`;
+  };
+
   const openRegisterModal = () => setIsRegisterModalOpen(true);
   const closeRegisterModal = () => setIsRegisterModalOpen(false);
 
@@ -96,53 +122,63 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="relative select-none flex flex-col h-screen bg-cover bg-center  bg-custom-bg">
+    <div className="relative select-none flex flex-col h-screen bg-cover bg-center bg-custom-bg">
       <div className="absolute inset-0 flex justify-center items-center">
-        <div className="flex flex-col items-center w-full max-w-md">
-          <div className="rounded-full bg-white bg-opacity-20 w-96 h-96 flex flex-col items-center justify-center p-10 mb-2">
-            <input
-              className="mb-1  mt-3 roboto rounded-lg p-2 w-full border-[#9e9e9e] border hover:border hover:w-full transition-all text-center"
-              type="text"
-              id="email"
-              placeholder="Entre com o seu email"
-              value={email}
-              onChange={handleUserChange}
-            />
+        <div className="rounded-lg bg-white bg-opacity-20 w-96 h-96 flex flex-col items-center justify-center p-10 mb-2">
+          <input
+            className="mb-1 mt-3 roboto rounded-lg p-2 w-full border-[#9e9e9e] border hover:border hover:w-full transition-all text-center"
+            type="text"
+            id="email"
+            placeholder="Entre com o seu email"
+            value={email}
+            onChange={handleUserChange}
+          />
 
-            <input
-              ref={passwordRef}
-              className="mb-3 mt-3 rounded-lg p-2 w-full border-[#9e9e9e] border hover:border hover:w-full transition-all text-center"
-              type="password"
-              id="senha"
-              placeholder="Entre com a sua senha"
-              value={senha}
-              onChange={handlePasswordChange}
-              onKeyPress={handleKeyPress}
-            />
+          <input
+            ref={passwordRef}
+            className="mb-3 mt-3 rounded-lg p-2 w-full border-[#9e9e9e] border hover:border hover:w-full transition-all text-center"
+            type="password"
+            id="senha"
+            placeholder="Entre com a sua senha"
+            value={senha}
+            onChange={handlePasswordChange}
+            onKeyPress={handleKeyPress}
+          />
 
-            {error && (
-              <div className="text-[#4CAF50] font-bold mt-2">{error}</div>
-            )}
+          {error && (
+            <div className="text-[#4CAF50] font-bold mt-2">{error}</div>
+          )}
+          <button
+            className="w-full bg-gradient-to-r from-teal-400 to-cyan-600 hover:from-teal-500 hover:to-cyan-700 text-white font-bold py-2 px-2 rounded-lg mb-2"
+            onClick={handleLogin}
+          >
+            Login
+          </button>
+          <button
+            className="w-full bg-white hover:bg-gray-100 text-gray-900 font-bold py-2 px-4 rounded-lg mb-2 flex items-center justify-center border border-gray-300"
+            onClick={handleGoogleLogin}
+          >
+            <img
+              src="https://img.icons8.com/color/20/000000/google-logo.png"
+              alt="Google logo"
+              className="mr-2"
+            />
+            Login com Google
+          </button>
+
+          <div className="flex justify-between w-full mt-4">
             <button
-              className="w-full bg-gradient-to-r from-teal-400 to-cyan-600 hover:from-teal-500 hover:to-cyan-700 text-white font-bold py-2 px-2 rounded-lg mb-2"
-              onClick={handleLogin}
+              className="rounded-lg px-2 bg-gradient-to-r from-teal-400 to-cyan-600 hover:from-teal-500 hover:to-cyan-700 text-white"
+              onClick={openRegisterModal}
             >
-              Login
+              Criar conta
             </button>
-            <div className="flex justify-between w-full mt-4">
-              <button
-                className=" rounded-lg px-2 bg-gradient-to-r from-teal-400 to-cyan-600 hover:from-teal-500 hover:to-cyan-700 text-white"
-                onClick={openRegisterModal}
-              >
-                Criar conta
-              </button>
-              <button
-                className="rounded-lg px-2 bg-gradient-to-r from-teal-400 to-cyan-600 hover:from-teal-500 hover:to-cyan-700 text-white"
-                onClick={openForgotPasswordModal}
-              >
-                Esqueci minha senha
-              </button>
-            </div>
+            <button
+              className="rounded-lg px-2 bg-gradient-to-r from-teal-400 to-cyan-600 hover:from-teal-500 hover:to-cyan-700 text-white"
+              onClick={openForgotPasswordModal}
+            >
+              Esqueci minha senha
+            </button>
           </div>
         </div>
       </div>
